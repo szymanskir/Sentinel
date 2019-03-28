6d3e127a4465c5e5f5278924e42732e4106f4625
szymanskir <ryszard.szymanski@outlook.com>
2019-03-28 16:53:21 +0100

    Create initial project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='sentinel',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project for Big Data clases at the Warsaw University of Technology',
+    author='Marcin Dardzińskim, Paweł Rzepiński, Sebastian Sowik, Ryszard Szymański',
+    license='MIT',
+)
