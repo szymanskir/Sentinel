07a97f3a175c4e79816c466893deacd40bfacdac
szymanskir <ryszard.szymanski@outlook.com>
2019-04-20 21:31:33 +0200

    Conver test data from pickle to jsonpickle

--- tests/test-connectors/test_historical_hn.py
+++ tests/test-connectors/test_historical_hn.py
@@ -3,29 +3,29 @@ from unittest.mock import patch
 
 from datetime import datetime
 from os.path import join, dirname, realpath
-from sentinel.utils import read_pickle
+from sentinel.utils import read_jsonpickle
 from sentinel.connectors.historical import HackerNewsHistoricalConnector
 
 MOCK_CONFIG = dict()
 
-def get_hn_comments_pkl():
-    hn_comments_pkl_path = join(dirname(realpath(__file__)), "hn_comments.pkl")
-    return read_pickle(hn_comments_pkl_path)
+def get_hn_comments_jsonpkl():
+    hn_comments_jsonpkl_path = join(dirname(realpath(__file__)), "hn-historical_comments.json")
+    return read_jsonpickle(hn_comments_jsonpkl_path)
 
 
 def mock_search():
-    return get_hn_comments_pkl()
+    return get_hn_comments_jsonpkl()
 
 
 @pytest.fixture
 def hn_comments():
-    return get_hn_comments_pkl()
+    return get_hn_comments_jsonpkl()
 
 
 @pytest.fixture
 def hacker_news_comment_json():
-    test_cases_dir = join(dirname(realpath(__file__)), "hacker_news_russia_json.pkl")
-    return read_pickle(test_cases_dir)
+    hn_comments_russia_json_pkl_path = join(dirname(realpath(__file__)), "hn-historical-russia.json")
+    return read_jsonpickle(hn_comments_russia_json_pkl_path)
 
 
 def test_HackerNewsMetadata_create_metadata_empty_points_json(hacker_news_comment_json):
