598dc0c85adf2d4bd63959f41a5f533ef080da6f
szymanskir <ryszard.szymanski@outlook.com>
2019-05-09 23:40:05 +0200

    Finish mock api

--- sentinel-WebApp/sentinel_backend/sentinel_backend/views.py
+++ sentinel-WebApp/sentinel_backend/sentinel_backend/views.py
@@ -1,9 +1,32 @@
-from flask import render_template
-
+from flask import jsonify, request, render_template
+from datetime import datetime
 from sentinel_backend import app
+from .repository import MockRepository
+
+_REPOSITORY = MockRepository('../mock-data')
 
 
 @app.route('/')
 def index():
     app.logger.warning('sample message')
     return render_template('index.html')
+
+
+@app.route('/mentions')
+def get_mentions():
+    since = datetime.strptime(request.args.get('from'), '%Y-%m-%d')
+    until = datetime.strptime(request.args.get('to'), '%Y-%m-%d')
+    keywords = request.args.get('keywords').split(',')
+    mentions = _REPOSITORY.get_mentions(since, until, keywords)
+    return jsonify(mentions)
+
+
+@app.route('/my-keywords')
+def get_my_keywords():
+    user = request.args.get('user')
+    return jsonify(_REPOSITORY.get_keywords(user=user))
+
+
+@app.route('/all-keywords')
+def get_all_keywords():
+    return jsonify(_REPOSITORY.get_keywords())
