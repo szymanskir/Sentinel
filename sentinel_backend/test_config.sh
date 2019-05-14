022e6f4e7b98517bf6d1e21215275442b5b7f08a
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2019-05-14 14:57:29 +0200

    Fix AWS credentials setting

--- sentinel_backend/test_config.sh
+++ sentinel_backend/test_config.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export AWS_ACCESS_KEY_ID=key
 export AWS_SECRET_ACCESS_KEY=secret
-export AWS_DEFAULT_REGION=us-west-1
-export DYNAMO_DB_URL='http://localhost:8000'
+export AWS_DEFAULT_REGION=eu-central-1
+export DYNAMO_DB_URL=http://localhost:8000
--- sentinel_spark/utils/setup.sh
+++ sentinel_spark/utils/setup.sh
@@ -1,3 +1,4 @@
 echo -e '\nexport PYSPARK_PYTHON=/shared/Sentinel/sentinel_spark/.env/bin/python3.6' >> /usr/hdp/current/spark2-client/conf/spark-env.sh
+echo -e '\nspark.executorEnv.AWS_ACCESS_KEY_ID KEY_ID\nspark.executorEnv.AWS_SECRET_ACCESS_KEY ACCESS_KEY\nspark.executorEnv.DYNAMO_DB_REGION eu-central-1\nspark.executorEnv.AWS_DEFAULT_REGION eu-central-1\n' >> /usr/hdp/current/spark2-client/conf/spark-defaults.conf
 var="log4j.rootCategory=WARN, console"
 sed -i "3s/.*/$var/" /usr/hdp/current/spark2-client/conf/log4j.properties
