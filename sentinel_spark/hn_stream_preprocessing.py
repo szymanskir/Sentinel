74abe990e88a4168e58042c78537314cedcca144
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2019-05-14 16:36:04 +0200

    Implement db writes for HN stream

--- sentinel_spark/hn_stream_preprocessing.py
+++ sentinel_spark/hn_stream_preprocessing.py
@@ -8,6 +8,7 @@ from sentinel_spark.data_manipulation.text_preprocessing import clean_article_te
 from sentinel_spark.data_manipulation.stream_utils import to_mention, clean_mention_text
 from textblob import TextBlob
 
+from sentinel_spark.db_writer import save_to_db
 
 sc = SparkContext.getOrCreate()
 sc.setLogLevel("WARN")
@@ -16,10 +17,17 @@ ssc = StreamingContext(sc, 3)
 kafka_params = {"bootstrap.servers": "sandbox-hdp.hortonworks.com:6667"}
 
 kafka_stream = KafkaUtils.createDirectStream(ssc, ["hacker-news"], kafka_params)
-hacker_news_mentions_cleaned = kafka_stream.map(lambda x: to_mention(x)).map(lambda x: clean_mention_text(x, clean_article_text))
-hacker_news_mentions_enriched = hacker_news_mentions_cleaned.map(lambda x: (x, TextBlob(x.text).sentiment.polarity))
+hacker_news_mentions_cleaned = kafka_stream.map(lambda x: to_mention(x)).map(
+    lambda x: clean_mention_text(x, clean_article_text)
+)
+hacker_news_mentions_enriched = hacker_news_mentions_cleaned.map(
+    lambda x: (x, TextBlob(x.text).sentiment.polarity)
+)
 
-hacker_news_mentions_enriched.pprint()
+
+output = hacker_news_mentions_enriched.mapPartitions(lambda x: save_to_db(x))
+
+output.pprint()
 
 ssc.start()
 ssc.awaitTermination()
