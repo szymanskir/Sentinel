e81fdeb77fd1e6d25a4d9ff1ed909a23174c0948
Marcin <mdardzinski96@gmail.com>
2019-05-15 21:04:44 +0200

    Fix crashes when no data meets search criteria

--- sentinel_backend/sentinel_backend/data_manipulation.py
+++ sentinel_backend/sentinel_backend/data_manipulation.py
@@ -2,19 +2,23 @@ import pandas as pd
 
 
 def get_keywords_mention_count(mentions_data: pd.DataFrame):
-    mention_count = (
-        mentions_data.groupby(by=["keyword", "date"]).size().reset_index(name="counts")
-    )
+    if len(mentions_data) == 0:
+        return pd.DataFrame(columns=["keyword", "date", "counts"])
 
-    return mention_count
+    return (
+        mentions_data.groupby(by=["keyword", "date"])
+        .size()
+        .reset_index(name="counts")
+        )
 
 
 def get_sentiment_scores(mentions_data: pd.DataFrame):
-    sentiment_scores = (
+    if len(mentions_data) == 0:
+        return pd.DataFrame(columns=["keyword", "date", "sentimentScore"])
+
+    return (
         mentions_data[["keyword", "date", "sentimentScore"]]
         .groupby(by=["keyword", "date"])
         .agg("mean")
         .reset_index()
     )
-
-    return sentiment_scores
