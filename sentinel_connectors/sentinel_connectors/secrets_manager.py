6378ec0f4510e88a95e37ea00cf3b2eb1f34f066
sowiks <sowiks@student.mini.pw.edu.pl>
2019-05-16 22:55:42 +0200

    Add logging to secrets manager

--- sentinel_connectors/sentinel_connectors/secrets_manager.py
+++ sentinel_connectors/sentinel_connectors/secrets_manager.py
@@ -6,11 +6,13 @@ import boto3
 import base64
 from botocore.exceptions import ClientError
 from abc import ABC
+import logging
 import json
 
 class SecretsManager(ABC):
     def __init__(self, secret_id: str):
-        self.secret_id = secret_id
+        self._secret_id = secret_id
+        self._logger = logging.getLogger(SecretsManager.__name__)
 
     def get_secrets(self):
 
@@ -29,9 +31,10 @@ class SecretsManager(ABC):
 
         try:
             get_secret_value_response = client.get_secret_value(
-                SecretId=self.secret_id
+                SecretId=self._secret_id
             )
         except ClientError as e:
+            
             if e.response['Error']['Code'] == 'DecryptionFailureException':
                 # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
                 # Deal with the exception here, and/or rethrow at your discretion.
@@ -52,10 +55,14 @@ class SecretsManager(ABC):
                 # We can't find the resource that you asked for.
                 # Deal with the exception here, and/or rethrow at your discretion.
                 raise e
+        except Exception as e:
+            self._logger.error(e)
+
         else:
             # Decrypts secret using the associated KMS CMK.
             # Depending on whether the secret is a string or binary, one of these fields will be populated.
             if 'SecretString' in get_secret_value_response:
+                self._logger.info(f"Secret with id: {self._secret_id} recovered successfully")
                 secret = get_secret_value_response['SecretString']
             else:
                 decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
