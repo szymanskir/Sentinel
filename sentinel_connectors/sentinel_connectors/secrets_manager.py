6c908af628f84687d3594cbf070ca32e3661c8fb
sowiks <sowiks@student.mini.pw.edu.pl>
2019-05-17 22:32:14 +0200

    Get region name from boto session, clean commments

--- sentinel_connectors/sentinel_connectors/secrets_manager.py
+++ sentinel_connectors/sentinel_connectors/secrets_manager.py
@@ -1,5 +1,5 @@
+# https://docs.aws.amazon.com/code-samples/latest/catalog/python-secretsmanager-secrets_manager.py.html
 import boto3
-from botocore.exceptions import ClientError
 from abc import ABC
 import logging
 import json
@@ -11,8 +11,8 @@ class SecretsManager(ABC):
         self._logger = logging.getLogger(SecretsManager.__name__)
 
     def get_secrets(self):
-        region_name = "eu-central-1"
-        # Create a Secrets Manager client
+        session = boto3.session.Session()
+        region_name = session.region_name
         session = boto3.session.Session()
         client = session.client(
             service_name='secretsmanager',
@@ -23,35 +23,10 @@ class SecretsManager(ABC):
             get_secret_value_response = client.get_secret_value(
                 SecretId=self._secret_id
             )
-        except ClientError as e:
-
-            if e.response['Error']['Code'] == 'DecryptionFailureException':
-                # Secrets Manager can't decrypt the protected secret text
-                # using the provided KMS key. Deal with the exception here,
-                # and/or rethrow at your discretion.
-                raise e
-            elif e.response['Error']['Code'] == 'InternalServiceErrorException':
-                # An error occurred on the server side.
-                # Deal with the exception here, and/or rethrow at your discretion.
-                raise e
-            elif e.response['Error']['Code'] == 'InvalidParameterException':
-                # You provided an invalid value for a parameter.
-                # Deal with the exception here, and/or rethrow at your discretion.
-                raise e
-            elif e.response['Error']['Code'] == 'InvalidRequestException':
-                # You provided a parameter value that is not valid for the
-                # current state of the resource. Deal with the exception here,
-                # and/or rethrow at your discretion.
-                raise e
-            elif e.response['Error']['Code'] == 'ResourceNotFoundException':
-                # We can't find the resource that you asked for.
-                # Deal with the exception here, and/or rethrow at your discretion.
-                raise e
         except Exception as e:
             self._logger.error(e)
             raise e
 
-        # Decrypts secret using the associated KMS CMK.
         self._logger.info(
             f"Secret with id: {self._secret_id} recovered successfully"
         )
