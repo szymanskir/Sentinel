24aa5eb0ea64da2b5e246e729e2af942c43f5b14
Marcin <mdardzinski96@gmail.com>
2019-05-31 21:39:28 +0200

    Add full frontent configuration

--- sentinel_web/src/App.tsx
+++ sentinel_web/src/App.tsx
@@ -1,19 +1,19 @@
 import * as React from "react";
 import { Dashboard } from "./Components/Dashboard";
 import Amplify from "@aws-amplify/core";
-import Auth from "@aws-amplify/auth";
 import { withAuthenticator } from "aws-amplify-react";
+import config from "config";
 
 Amplify.configure({
   Auth: {
-    region: "eu-central-1",
-    userPoolId: "eu-central-1_mADHJQzc7",
-    userPoolWebClientId: "2r49jo9f4mf92aspdpemmp2u1v"
+    region: config.awsRegion,
+    userPoolId: config.cognitoUserPoolId,
+    userPoolWebClientId: config.cognitoWebClientId
   }
 });
 
 const App: React.FC = () => {
-  return <Dashboard/>;
+  return <Dashboard />;
 };
 
 export default withAuthenticator(App, true);
--- sentinel_web/src/Configuration/Config.dev.ts
+++ sentinel_web/src/Configuration/Config.dev.ts
@@ -1,4 +1,7 @@
 const config: Configuration = {
-    apiEndpoint: "http://localhost:5000"
+    apiEndpoint: "http://localhost:5000",
+    awsRegion: "eu-central-1",
+    cognitoUserPoolId: "eu-central-1_mADHJQzc7",
+    cognitoWebClientId: "2r49jo9f4mf92aspdpemmp2u1v"
 };
 export default config;
