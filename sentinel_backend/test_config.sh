b59053d88a249bf66d69cfea180e7d9315a88069
Marcin <mdardzinski96@gmail.com>
2019-05-31 22:51:14 +0200

    Correct development config

--- sentinel_backend/test_config.sh
+++ sentinel_backend/test_config.sh
@@ -5,5 +5,5 @@ export AWS_DEFAULT_REGION=eu-central-1
 export DYNAMO_DB_URL=http://localhost:8000
 export DYNAMO_DB_REGION=eu-central-1
 export COGNITO_REGION="eu-central-1"
-export COGNITO_USERPOOL_ID="eu-central-1_mADHJQzc7"
-export COGNITO_APP_CLIENT_ID="2r49jo9f4mf92aspdpemmp2u1v"
+export COGNITO_USERPOOL_ID="eu-central-1_CKkBoqFRM"
+export COGNITO_APP_CLIENT_ID="5gho8rqvh9oblnjegjukhsv0o9"
