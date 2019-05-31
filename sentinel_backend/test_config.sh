fffdfd1294ad8d75a9bf26da7123da592753e514
Marcin <mdardzinski96@gmail.com>
2019-05-31 21:51:26 +0200

    Add stub env variables, remove hardcoded config from backend

--- sentinel_backend/test_config.sh
+++ sentinel_backend/test_config.sh
@@ -4,3 +4,6 @@ export AWS_SECRET_ACCESS_KEY=secret
 export AWS_DEFAULT_REGION=eu-central-1
 export DYNAMO_DB_URL=http://localhost:8000
 export DYNAMO_DB_REGION=eu-central-1
+export COGNITO_REGION="eu-central-1"
+export COGNITO_USERPOOL_ID="eu-central-1_mADHJQzc7"
+export COGNITO_APP_CLIENT_ID="2r49jo9f4mf92aspdpemmp2u1v"
